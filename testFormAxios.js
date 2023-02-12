e2453dd395d93a621664a38bfb81d2072f96d9db
Sakib <Sakibsaizeed@gmail.com>
2023-02-12 20:47:31 +0600

    Update testFormAxios.js

--- testFormAxios.js
+++ testFormAxios.js
@@ -61,3 +61,60 @@ const RegistrationForm = () => {
 };
 
 export default RegistrationForm;
+//////////////
+/////////////////////////////////////////////////////////
+//////////////////////////////////////
+import React, { useState, useEffect } from 'react';
+import { useForm } from 'react-hook-form';
+import { TextField, Button } from '@material-ui/core';
+
+const Form = () => {
+  const { handleSubmit, register, errors } = useForm();
+  const [data, setData] = useState({});
+
+  const onSubmit = async (formData) => {
+    try {
+      const response = await fetch('https://jsonplaceholder.typicode.com/posts', {
+        method: 'POST',
+        body: JSON.stringify(formData),
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      });
+      const result = await response.json();
+      setData(result);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  return (
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <TextField
+        name="title"
+        label="Title"
+        inputRef={register({ required: true })}
+        error={Boolean(errors.title)}
+        helperText={errors.title && 'Title is required'}
+        fullWidth
+        margin="normal"
+        variant="outlined"
+      />
+      <TextField
+        name="body"
+        label="Body"
+        inputRef={register({ required: true })}
+        error={Boolean(errors.body)}
+        helperText={errors.body && 'Body is required'}
+        fullWidth
+        margin="normal"
+        variant="outlined"
+      />
+      <Button type="submit" color="primary" variant="contained">
+        Submit
+      </Button>
+    </form>
+  );
+};
+
+export default Form;
